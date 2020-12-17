0172e6297c8cd5f7536c491a7a268dd3acce7e44
azizmousa <azizmousa1010@gmail.com>
2020-12-17 23:59:49 +0200

    remove the extra constructor

--- Node.py
+++ Node.py
@@ -2,11 +2,6 @@ class Node:
 	value = None
 	next_ptr = None
 	
-	def __init__(self, value):
+	def __init__(self, value = None):
 		self.value = value
-		self.next_ptr = None
-
-
-	def __init__(self):
-		self.value = None
 		self.next_ptr = None
