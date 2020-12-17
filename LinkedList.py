6a27f77b2560c65e0f5c0f5b0c1e790f4e2d75b5
azizmousa <azizmousa1010@gmail.com>
2020-12-17 23:28:20 +0200

    create add_first, display functions

--- LinkedList.py
+++ LinkedList.py
@@ -1,2 +1,24 @@
+from Node import Node
+
+
 class LinkedList:
-	pass
+
+	def __init__(self):
+		self.__head = None
+		self.__tail = None
+
+	def add_first(self, value):
+		node = Node()
+		node.value = value
+		if self.__head == None:
+			self.__head = node
+			self.__tail = node
+		else:
+			node.next_ptr = self.__head
+			self.__head = node
+
+	def display(self):
+		itrator = self.__head
+		while itrator != None:
+			print(itrator.value)
+			itrator = itrator.next_ptr
